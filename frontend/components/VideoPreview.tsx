4b6594c9943708284441459bbfb84ed42fbecc96
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2026-03-31 09:42:58 +0800

    fix: update Video Preview

--- frontend/components/VideoPreview.tsx
+++ frontend/components/VideoPreview.tsx
@@ -477,7 +477,7 @@ export default function VideoPreview({
               )}
               title="Toggle Frame Preview"
             >
-              �️
+              📷
             </button>
             
             <select
